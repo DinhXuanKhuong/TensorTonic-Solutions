468644b5825e3c892780a950fe9a598656331acb
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-05-02 11:15:35 +0700

    Update solution for make-diagonal

--- make-diagonal/make-diagonal.py
+++ make-diagonal/make-diagonal.py
@@ -7,9 +7,13 @@ def make_diagonal(v):
     # Write code here
     # pass
     v = np.array(v)
-    n = len(v)
-    res = np.zeros((n,n))
+    # n = len(v)
+    # res = np.zeros((n,n))
 
-    for i in range(n):
-        res[i, i] = v[i]
-    return res
+    # for i in range(n):
+    #     res[i, i] = v[i]
+
+
+    diag_matrix = np.diag(v) #can use like this LOL =))
+    
+    return diag_matrix
