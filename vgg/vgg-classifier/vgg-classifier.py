263e0e65526d2fcf52be1c4b47bf03fc0a303b2b
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-05-03 11:37:31 +0700

    Add solution for vgg-classifier

---
+++ vgg/vgg-classifier/vgg-classifier.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def vgg_classifier(features: np.ndarray, W1: np.ndarray, b1: np.ndarray,
+                   W2: np.ndarray, b2: np.ndarray, W3: np.ndarray, b3: np.ndarray) -> np.ndarray:
+    """
+    Returns: np.ndarray of shape (B, num_classes) with classification logits
+    """
+    # Your implementation here
+    # pass
+    B, H, W, C = features.shape
+    x = features.reshape(B, -1)
+
+    x = np.maximum(0, x @ W1 + b1)
+    x = np.maximum(0, x @ W2 + b2)
+    x = x @ W3 + b3
+
+    return x
