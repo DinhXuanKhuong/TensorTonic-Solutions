dc1a666e8dc6ab17b01b92f658d675d04d2cce4c
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-05-16 23:50:23 +0700

    Add solution for max-pooling-2d

---
+++ max-pooling-2d/max-pooling-2d.py
@@ -0,0 +1,24 @@
+import numpy as np 
+
+def max_pooling_2d(X, pool_size):
+    """
+    Apply 2D max pooling with non-overlapping windows.
+    """
+    # Write code here
+    X = np.array(X)
+
+    h, w = X.shape
+
+    h_out = h // pool_size
+    w_out = w // pool_size
+
+    res = np.zeros((h_out, w_out))
+    for i in range(h_out):
+        for j in range(w_out):
+            arr = X[i * pool_size: (i+1)*pool_size, j * pool_size: (j + 1) * pool_size]
+
+            res[i, j] = np.max(arr)
+    return res.tolist()
+            
+
+    
