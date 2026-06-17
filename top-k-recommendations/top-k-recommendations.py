22e593db1d80f048f5023dde9cfb20e05d8317f4
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-06-17 20:33:21 +0700

    Solve top-k-recommendations

--- top-k-recommendations/top-k-recommendations.py
+++ top-k-recommendations/top-k-recommendations.py
@@ -5,15 +5,9 @@ def top_k_recommendations(scores, rated_indices, k):
     """
     # Write code here
     scores = np.asarray(scores, dtype = np.float64)
-    # [5,1,3] -> [-inf, 1, 3] -> [inf, -1, -3] -> [1, 2, 0 ] ->  
-    if (len(rated_indices)):
-        print("hello")
-        scores[list(rated_indices)] = -np.inf
-        
-    # print(scores)
-    idx = np.argsort(-scores).tolist()
+    scores[list(rated_indices)] = -np.inf
     
-
+    idx = np.argsort(-scores).tolist()
     k = min(k, len(scores) - len(rated_indices))
 
     return idx[:k]
