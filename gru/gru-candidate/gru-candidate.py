560777a3e862ac74c39f167a9c04edb0b9fdddf5
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-07-20 10:51:01 +0700

    Solve gru-candidate

---
+++ gru/gru-candidate/gru-candidate.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def candidate_hidden(h_prev: np.ndarray, x_t: np.ndarray, r_t: np.ndarray,
+                     W_h: np.ndarray, b_h: np.ndarray) -> np.ndarray:
+    """
+    Compute candidate: h_tilde = tanh(W_h @ [r*h, x] + b_h)
+    """
+    # YOUR CODE HERE
+    # pass
+    h_tilde = np.tanh(np.concat([r_t * h_prev, x_t], axis = -1) @W_h.T + b_h)
+    return h_tilde
