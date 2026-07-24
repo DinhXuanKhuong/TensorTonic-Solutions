e7a21fcee1c94a422ce1a094253b16f2bbd1517b
Dinh Xuan Khuong <150417584+DinhXuanKhuong@users.noreply.github.com>
2026-07-24 18:58:42 +0700

    Solve lstm-full-network

---
+++ lstm/lstm-full-network/lstm-full-network.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-np.clip(x, -500, 500)))
+
+class LSTM:
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int):
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        scale = np.sqrt(2.0 / (input_dim + hidden_dim))
+
+        self.W_f = np.random.randn(hidden_dim, hidden_dim + input_dim) * scale
+        self.W_i = np.random.randn(hidden_dim, hidden_dim + input_dim) * scale
+        self.W_c = np.random.randn(hidden_dim, hidden_dim + input_dim) * scale
+        self.W_o = np.random.randn(hidden_dim, hidden_dim + input_dim) * scale
+        self.b_f = np.zeros(hidden_dim)
+        self.b_i = np.zeros(hidden_dim)
+        self.b_c = np.zeros(hidden_dim)
+        self.b_o = np.zeros(hidden_dim)
+
+        self.W_y = np.random.randn(output_dim, hidden_dim) * np.sqrt(2.0 / (hidden_dim + output_dim))
+        self.b_y = np.zeros(output_dim)
+
+    def forward(self, X: np.ndarray) -> tuple:
+        """
+        Forward pass. Returns (y, h_last, C_last).
+        """
+        # YOUR CODE HERE
+        # pass
+        N, T, input_dim = X.shape 
+        y = np.zeros((N, T, self.output_dim))
+        h_prev = np.zeros((N, self.hidden_dim))
+        C_prev = np.zeros((N, self.hidden_dim))
+        # h_t = None
+        
+        for t in range(T):
+            x_t = X[:, t, :]
+            f_t = sigmoid(np.concat([h_prev, x_t], axis = -1) @ self.W_f.T + self.b_f)
+            i_t = sigmoid(np.concat([h_prev, x_t], axis = -1) @ self.W_i.T + self.b_i)
+            C_tidle = np.tanh(np.concat([h_prev, x_t], axis = -1) @ self.W_c.T + self.b_c)
+        
+            o_t = sigmoid(np.concat([h_prev, x_t], axis = -1) @ self.W_o.T + self.b_o)
+        
+            C_t = f_t * C_prev + i_t * C_tidle
+            h_t = o_t * np.tanh(C_t)
+            y[:, t, :] =   h_t @ self.W_y.T + self.b_y 
+
+            h_prev = h_t
+            C_prev = C_t
+        
+        return y, h_prev, C_prev
